104018bfd70c1ea6fd7133d3efa82857d240503e
Andrey A <drew.leks@gmail.com>
2020-05-05 20:07:37 +0300

    add project

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,21 @@
+import game_functions as gf
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(ai_settings, screen)
+
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
